a90d3311a99bb98d29389d83a20125c55349a5b9
Tomas Petricek <tpetricek@gmail.com>
2019-02-13 10:19:28 +0100

    Moved to common/exploration repo.

--- map_exploration/launch/send_goal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rostopic pub -r 2 mapping/goals nav_msgs/Path  '{header: {frame_id: camera_depth_optical_frame}, poses: [ {pose: {position: {x: 0, y: 0}}}]  }'
-
-
--- map_exploration/launch/startRVIZ.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# this launches rviz with custom-made config file, which is more suitable for debugging purposes
-rosrun rviz rviz -d "$(rospack find robot_mapping)/launch/mappingRVIZdebug2.rviz"
-
