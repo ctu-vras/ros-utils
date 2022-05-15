192631d86cc8f35c8b08abeccde2dc4d9a6e8b8e
Martin Pecka <peci1@seznam.cz>
2022-05-15 02:10:42 +0200

    Rename test targets so that their names do not conflict with other projects.

--- cras_cpp_common/test/test_xmlrpc_value_utils.cpp
+++ cras_cpp_common/test/test_xmlrpc_value_utils.cpp
@@ -380,8 +380,7 @@ TEST(XmlRpcValueUtils, ConvertMapArrays)
     x, false);}
 }
 
-
-  int main(int argc, char **argv)
+int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
