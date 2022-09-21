6912a98e8dd9b02f997aa1b370f60bf531fbbbed
Martin Pecka <peci1@seznam.cz>
2022-09-21 09:36:50 +0200

    cras_py_common: Fixed installspace.

--- cras_py_common/setup.py
+++ cras_py_common/setup.py
@@ -9,7 +9,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['cras'],
+    packages=['cras', 'cras.impl'],
     package_dir={'': 'src'},
 )
 
