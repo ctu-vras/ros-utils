a2bcf0fc2807b4cb863681c61b5ad62da04b78b5
Martin Pecka <peckama2@fel.cvut.cz>
2023-03-06 02:52:28 +0100

    cras_docs_common: Fix providing the custom sphinx theme to packages from this stack on ROS buildfarm.

--- cras_docs_common/setup.py
+++ cras_docs_common/setup.py
@@ -11,6 +11,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 setup_args = generate_distutils_setup(
     packages=['cras_docs_common'],
     package_dir={'': 'src'},
+    # The following is needed to provide the theme files for the other packages in this stack during doc jobs on buildfarm (it ignores the install() directive in CMakeLists.txt)
+    package_data={'cras_docs_common': ['sphinx-themes/*', 'sphinx-themes/*/*', 'sphinx-themes/*/*/*', 'sphinx-themes/*/*/*/*']},
 )
 
 setup(**setup_args)
